cf0c84b3536305e573ab3e64e38521f4851855c0
Radek <radgra2@gmail.com>
2019-05-04 18:07:53 +0200

    settings changed

--- app/settings.py
+++ app/settings.py
@@ -138,3 +138,9 @@ from easy_thumbnails.conf import Settings as thumbnail_settings
 THUMBNAIL_PROCESSORS = (
     'image_cropping.thumbnail_processors.crop_corners',
 ) + thumbnail_settings.THUMBNAIL_PROCESSORS
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
